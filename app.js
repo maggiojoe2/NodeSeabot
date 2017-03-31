63683d9bc6d5621cdbcb7fdf5f25c943aa49cc04
maggiojoe2 <maggiojoe2@tamu.edu>
2017-03-30 20:17:58 -0500

    Added bot tracker function.

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-var tmi = require('tmi.js');
+/* - - - - - - - - - - - - - - - - - - - - - - - -
+    Seab0t NodeJS for Twitch
+    Joe Maggio
+ - - - - - - - - - - - - - - - - - - - - - - - - */
 
-var channellist = ["thesealion95", "seab0t95"];
+var tmi = require('tmi.js');
+var channellist = ["thesealion95"];
+
+/* - - - - - - - - - - - - - - - - - - - - - - - -
+    Connection Stuff
+ - - - - - - - - - - - - - - - - - - - - - - - - */
 
 var options = {
     options: {
-        debug: false
+        debug: true
     },
     connection: {
         cluster: "aws",
@@ -20,13 +28,46 @@ var options = {
 var client = new tmi.client(options);
 client.connect();
 
+/* - - - - - - - - - - - - - - - - - - - - - - - -
+    Event Listeners
+ - - - - - - - - - - - - - - - - - - - - - - - - */
+
 client.on('connected', function(address, port) {
     client.whisper("thesealion95", "Seab0t connected to " + channellist);
     client.log.info("Address: " + address + " Port: " + port);
-    // client.action(address, "Joined Channel.");
+    // client.action("thesealion95", "Joined Channel.");
 });
 
 client.on('disconnected', function(reason) {
-    console.log("Reconnecting...");
+    client.log.info("Reconnecting...");
     client.connect();
 });
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - -
+    Request for Chatters API call
+ - - - - - - - - - - - - - - - - - - - - - - - - */
+
+var request = require("request");
+var url = "https://tmi.twitch.tv/group/user/nalcs1/chatters";
+
+var getChatters = setInterval(function() {
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            regexp = new RegExp(/thesealion\d{2,}/);
+            users = body.chatters.viewers;
+            // console.log(users);
+            for (i = 0; i < users.length; i++) {
+                var check = regexp.test(users[i]);
+                if (check) {
+                    client.whisper("thesealion95", "Found " + users[i] + " in the chat.");
+                    client.whisper("thesealion95", "use: /ban " + users[i]);
+                }
+            }
+        }
+    });    
+}, 60000);
